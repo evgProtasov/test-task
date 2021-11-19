4a6f725d9e0821d0ca30baf23d1ab38509353ee0
evgProtasov <89400572+evgProtasov@users.noreply.github.com>
2021-11-19 20:45:45 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 'use strict';
 
+$( function() {
+    $("#slider-range").slider({
+      range: true,
+      step: 100,
+      min: 1000,
+      max: 50000,
+      values: [ 1200, 30000 ],
+      slide: function( event, ui ) {
+        $("#amount1").val("от " + ui.values[0]);
+        $("#amount2").val("до " + ui.values[1]);
+      }
+    });
+    $( "#amount1" ).val("от " + $( "#slider-range" ).slider("values", 0 ));
+    $( "#amount2" ).val("до " +$( "#slider-range" ).slider( "values", 1 ));
+  });
+   
 
 function pageWidth() {
     var widthWindow = window.innerWidth;
